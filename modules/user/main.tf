ac1359c95341487992e1c048461dd9b9c0dc232e
Petko D. Petkov <pdp@gnucitizen.org>
2018-10-23 13:06:21 +0100

    Explicitly deny access.

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -3,7 +3,24 @@ resource "aws_iam_user" "user" {
   path = "${var.path}"
 }
 
+resource "aws_iam_user_policy" "policy" {
+  user = "${aws_iam_user.user.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Deny",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_access_key" "access_key" {
-  count = "${var.tokens}"
   user  = "${aws_iam_user.user.name}"
+  count = "${var.tokens}"
 }
